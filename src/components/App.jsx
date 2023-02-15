17e8298c88db0b6c61792555a59199c47e03bd64
donCelis <alexcelis019@gmail.com>
2023-02-14 21:23:12 -0500

    recibir objetos o arreglos por props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,23 @@ import ProductCard from './ProductCard'
 import { products } from '../../db.json'
 
 function App () {
-  const mappingProducts = products.map(({ id, title, stock, price, thumbnail }) => ({
-    id,
-    price,
-    name: title,
-    count: stock,
-    photo: thumbnail
-  }))
-
-  console.log(mappingProducts)
+  const mappingProducts = products.map(
+    ({ title, stock, price, thumbnail, images }, index) => ({
+      /* no deberían usar el index de un array como id */
+      id: index,
+      price,
+      name: title,
+      count: stock,
+      photo: thumbnail,
+      images
+    })
+  )
 
   return (
     <section className='container py-5'>
       <h2 className='text-center mb-5'>Lista de productos</h2>
       <section className='row'>
-        {mappingProducts.map(product => (
+        {mappingProducts.map((product) => (
           <aside key={product.id} className='col-4'>
             <ProductCard {...product} />
           </aside>
