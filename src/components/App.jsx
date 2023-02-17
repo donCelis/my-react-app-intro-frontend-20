c16625bfe06c03e0eb912ca87cc8c926c2e6b078
donCelis <alexcelis019@gmail.com>
2023-02-16 22:30:40 -0500

    creando un custom hook, configurando axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,11 @@
+import { useProducts } from '../hooks/useProducts'
 import ProductCard from './ProductCard'
-import { useEffect, useState } from 'react'
-
-const LIMIT = 9
-const SELECT_PROPS = 'id,title,stock,price,thumbnail'
-
-const API_URL = `https://dummyjson.com/products?limit=${LIMIT}&select=${SELECT_PROPS}`
+// import { products as mockData } from '../../db.json'
 
 function App () {
-  const [products, setProducts] = useState([])
+  const { products, isLoading, errors } = useProducts()
 
-  const fetchProducts = async () => {
-    const request = await fetch(API_URL)
-    const { products } = await request.json()
-    setProducts(products)
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
-
-  const mappingProducts = products.map(
+  const mappingProducts = !errors && products.map(
     (product) => ({
       ...product,
       name: product.title,
@@ -31,13 +17,16 @@ function App () {
   return (
     <section className='container py-5'>
       <h2 className='text-center mb-5'>Lista de productos</h2>
-      <section className='row gy-4'>
-        {mappingProducts.map((product) => (
-          <aside key={product.id} className='col-4'>
-            <ProductCard {...product} />
-          </aside>
-        ))}
-      </section>
+      {isLoading && <p className='text-center'>...Loading</p>}
+      {!isLoading && (
+        <section className='row gy-4'>
+          {mappingProducts.map((product) => (
+            <aside key={product.id} className='col-4'>
+              <ProductCard {...product} />
+            </aside>
+          ))}
+        </section>
+      )}
     </section>
   )
 }
---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+import { instance } from './instance'
+
+export const getProducts = async ({ limit = 10, select = 'id' }) => {
+  const { data } = await instance.get(`/products?limit=${limit}&select=${select}`)
+  return data
+}
